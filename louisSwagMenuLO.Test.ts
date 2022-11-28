060efdd0a4a53a4d8a60ae85e2ebe5e53346804c
louischarleshaynes <louisch123@yahoo.com>
2022-11-28 14:50:47 -0800

    Second Push-New code

--- louisSwagMenuLO.Test.ts
+++ louisSwagMenuLO.Test.ts
@@ -15,13 +15,23 @@ import { swagMenuLO} from "./louisSwagPOM";
         })
     
         test ("logout from the all items page", async () =>{
-            
+            /*
             await menPage.setInput(menPage.addUserName, "standard_user");
             await menPage.setInput(menPage.addPassword, "secret_sauce");
             await menPage.click(menPage.loginBtn);
             await menPage.click(menPage.menuBtn);
             await menPage.click(menPage.logoutBtn);// Need to find the corret selector. how use xpath for this one
-
+            */
+            await driver.findElement(menPage.addUserName).click();
+            await driver.findElement(menPage.addUserName).clear();
+            await driver.findElement(menPage.addUserName).sendKeys("standard_user");
+            await driver.findElement(menPage.addPassword).click();
+            await driver.findElement(menPage.addPassword).clear();
+            await driver.findElement(menPage.addPassword).sendKeys("secret_sauce");
+            await driver.findElement(menPage.loginBtn).click();
+            await driver.findElement(menPage.inventoryItem).click();
+            await driver.findElement(menPage.menuBtn).click();
+            await driver.findElement(menPage.logoutBtn).click();
 
 
         });
